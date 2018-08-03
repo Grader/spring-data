9a7254135e68a9aa88c487a68cedf7f62c29c68e
Yuriy Stolbov <labcontext@gmail.com>
2018-08-03 21:11:14 +0400

    6 cart del

--- src/main/java/com/example/sweater/repos/CartRepo.java
+++ src/main/java/com/example/sweater/repos/CartRepo.java
@@ -1,17 +1,11 @@
 package com.example.sweater.repos;
 
 import com.example.sweater.domain.Cart;
-import com.example.sweater.domain.Good;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CartRepo extends JpaRepository<Cart, Long> {
 
     List<Cart> findByUserId(Long id);
-
-    @Query("delete from Cart c where ")
-    void deleteGoodInCart(Long id);
-
 }
